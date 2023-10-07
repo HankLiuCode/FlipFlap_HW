8cff031b0914d63f5322f9efd04e77ac804d314f
HankLiuCode <hankliucode@gmail.com>
2023-10-08 01:08:23 +0800

    first commit

---
+++ tsv_buddy.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Module that can be included (mixin) to take and output TSV data
+module TsvBuddy
+  # Converts a String with TSV data into internal data structure @data
+  # arguments: tsv - a String in TSV format
+  # returns: nothing
+  def take_tsv(tsv)
+
+  end
+
+  # Converts @data into tsv string
+  # arguments: none
+  # returns: String in TSV format
+  def to_tsv
+
+  end
+end
